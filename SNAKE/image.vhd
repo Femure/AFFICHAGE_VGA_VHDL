59bbd5ae9058bfaedc5f35f4435dddec6ef7fa18
Femure <maxime.femery@gmail.com>
2024-04-12 16:07:00 +0200

    Pb RNG

--- SNAKE/image.vhd
+++ SNAKE/image.vhd
@@ -4,14 +4,14 @@ USE IEEE.std_logic_1164.ALL;
 ENTITY image IS
     PORT (
         RST, BLANK : IN STD_LOGIC;
-        HCOUNT, VCOUNT : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+        IS_FOOD : IN STD_LOGIC;
         RED, GREEN, BLUE : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
 END image;
 
 ARCHITECTURE rtl OF image IS
 BEGIN
-    PROCESS (RST, BLANK, HCOUNT, VCOUNT)
+    PROCESS (RST, BLANK, IS_FOOD)
     BEGIN
         IF (RST = '1') THEN
             RED <= (OTHERS => '0');
@@ -19,9 +19,19 @@ BEGIN
             BLUE <= (OTHERS => '0');
         ELSE
             IF (BLANK = '1') THEN
-                RED <= "1111";
+                IF (IS_FOOD = '1') THEN -- Rouge
+                    RED <= "1111";
+                    GREEN <= "0000";
+                    BLUE <= "0000";
+                ELSE
+                    RED <= "0000";
+                    GREEN <= "0000";
+                    BLUE <= "0000";
+                END IF;
             ELSE
                 RED <= "0000";
+                GREEN <= "0000";
+                BLUE <= "0000";
             END IF;
         END IF;
     END PROCESS;
--- SNAKE/snake.vhd
+++ SNAKE/snake.vhd
@@ -27,16 +27,35 @@ ARCHITECTURE structural OF snake IS
         );
     END COMPONENT;
 
+    COMPONENT clk_food_respawn IS
+        PORT (
+            FRAME, RST : IN STD_LOGIC;
+            CLK_RESPAWN : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
+    COMPONENT food_spawn IS
+        PORT (
+            RST, CLK_RESPAWN, FRAME : IN STD_LOGIC;
+            X_SNAKE, Y_SNAKE : IN INTEGER;
+            HCOUNT, VCOUNT : IN STD_LOGIC_VECTOR(10 DOWNTO 0);
+            IS_EATEN, IS_FOOD : OUT STD_LOGIC
+        );
+    END COMPONENT;
+
     COMPONENT image IS
         PORT (
             RST, BLANK : IN STD_LOGIC;
+            IS_FOOD : IN STD_LOGIC;
             RED, GREEN, BLUE : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
         );
     END COMPONENT;
 
-    SIGNAL pixel_clk : STD_LOGIC;
+    SIGNAL pixel_clk, clk_respawn : STD_LOGIC;
     SIGNAL blank, frame : STD_LOGIC;
     SIGNAL hcount, vcount : STD_LOGIC_VECTOR(10 DOWNTO 0);
+    SIGNAL x_snake, y_snake : INTEGER;
+    SIGNAL is_eaten, is_food : STD_LOGIC;
 
 BEGIN
 
@@ -44,7 +63,11 @@ BEGIN
     A0 : div_25MHz PORT MAP(CLK => CLK, RST => RST, PIXEL_CLK => pixel_clk);
     A1 : vga_controller_640_60 PORT MAP(PIXEL_CLK => pixel_clk, RST => RST, HS => HS, VS => VS, BLANK => blank, FRAME => frame, HCOUNT => hcount, VCOUNT => vcount);
 
+    -- Gestion des cubes de nourriture
+    N0 : clk_food_respawn PORT MAP(FRAME => frame, RST => RST, CLK_RESPAWN => clk_respawn);
+    N1 : food_spawn PORT MAP(RST => RST, CLK_RESPAWN => clk_respawn, FRAME => frame, X_SNAKE => x_snake, Y_SNAKE => y_snake, HCOUNT => hcount, VCOUNT => vcount, IS_EATEN => is_eaten, IS_FOOD => is_food);
+
     -- Rendu final sur l'écran
-    A2 : image PORT MAP(RST => RST, BLANK => blank, RED => RED, GREEN => GREEN, BLUE => BLUE);
+    A2 : image PORT MAP(RST => RST, BLANK => blank, IS_FOOD => is_food, RED => RED, GREEN => GREEN, BLUE => BLUE);
 
 END structural;
