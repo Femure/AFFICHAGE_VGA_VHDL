51897aa3f695d27cb055d6f205883cba05f35cda
aminallali <132190544+aminallali@users.noreply.github.com>
2024-04-20 22:19:12 +0200

    image_test_4

---
+++ VGA 3 COULEURS/tb_vga_3_chromes.vhd
@@ -0,0 +1,48 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+
+ENTITY tb_vga_3_monochrome IS
+END tb_vga_3_monochrome;
+
+ARCHITECTURE testbench OF tb_vga_3_monochrome IS
+
+    COMPONENT vga_3_monochrome IS
+        PORT (
+            CLK, RST : IN STD_LOGIC;
+            HS, VS : OUT STD_LOGIC;
+            RED, GREEN, BLUE : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+        );
+    END COMPONENT;
+
+    SIGNAL CLK, RST, HS, VS : STD_LOGIC;
+    SIGNAL RED, GREEN, BLUE : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+BEGIN
+
+    DUT0 : vga_3_monochrome PORT MAP(
+        CLK => CLK,
+        RST => RST,
+        HS => HS,
+        VS => VS,
+        RED => RED,
+        GREEN => GREEN,
+        BLUE => BLUE 
+    );
+
+    stimulus0 : PROCESS
+    BEGIN
+        RST <= '1';
+        WAIT FOR 50 ns;
+        RST <= '0';
+        WAIT;
+    END PROCESS stimulus0;
+
+    CLK_0 : PROCESS
+    BEGIN
+        CLK <= '0';
+        WAIT FOR 10 ns;
+        CLK <= '1';
+        WAIT FOR 10 ns;
+    END PROCESS CLK_0;
+
+END testbench;
