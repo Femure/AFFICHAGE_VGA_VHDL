c6e6be65b7d9ae9efaf465a70ade7d7f7b607c16
Femure <maxime.femery@gmail.com>
2024-05-17 00:18:54 +0200

    Init yoshi nightmare

--- PONG/PONG/PONG.runs/synth_1/pong.tcl
+++ PONG/PONG/PONG.runs/synth_1/pong.tcl
@@ -72,7 +72,6 @@ proc create_report { reportName command } {
 OPTRACE "synth_1" START { ROLLUP_AUTO }
 set_param chipscope.maxJobs 4
 set_param xicom.use_bs_reader 1
-set_msg_config -id {Common 17-41} -limit 10000000
 set_msg_config  -id {Synth 8-327}  -string {{WARNING: [Synth 8-327] inferring latch for variable 'IS_SNAKE_reg' [C:/Users/maxim/VHDL/FPGA/AFFICHAGE VGA/SNAKE/serpent/snake_move.vhd:93]}}  -suppress 
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
